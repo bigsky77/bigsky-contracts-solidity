546f895cb9690be3fab8d788dd7c6863c17d9554
bigsky77 <simonjudd2@gmail.com>
2022-11-21 14:43:09 +0200

    fix: comments/body

--- src/BigSky.sol
+++ src/BigSky.sol
@@ -1,16 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { Ship } from './ships/Ship.sol';
+
 contract BigSky {
   address immutable owner;
 
+  /*//////////////////////////////////////////////////////////////
+                               EVENTS
+  //////////////////////////////////////////////////////////////*/
+
   event GameStart(bool _isStarted);
+  event PlayerMove(uint256 _positionX, uint256 _positionyY);
+
+  /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+  //////////////////////////////////////////////////////////////*/
 
   constructor(){
     owner = msg.sender;
   }
-
-  bool isStarted;
+ 
+  /*//////////////////////////////////////////////////////////////
+                              MODIFIERS
+  //////////////////////////////////////////////////////////////*/
 
   modifier onlyOwner() {
     require(msg.sender == owner, 'NOT OWNER');
@@ -18,27 +31,55 @@ contract BigSky {
     _;
   }
 
+  /*//////////////////////////////////////////////////////////////
+                             GAME STATE
+  //////////////////////////////////////////////////////////////*/
+
+  bool isStarted;
+
+  enum ShipType{
+    PLAYER,
+    ENEMY,
+    STAR
+  } 
+
+  ShipType ship_type; 
+
   struct ShipData {
     uint256 positionX;
     uint256 positionY;
+    ShipType ship_type;
   }
   ShipData ship;
+
+  /*//////////////////////////////////////////////////////////////
+                               SETUP
+  //////////////////////////////////////////////////////////////*/
 
   function startGame() public {
     isStarted = true;
     
+    setPosition();
     emit GameStart(isStarted);
   }
 
-  function setPosition() public {
+  function setPosition() internal {
     uint256 x = 0;
     uint256 y = 0;
-    ship = ShipData(x, y); 
+    ship = ShipData(x, y, ship_type); 
+
+    emit PlayerMove(x, y);
   }
+
+  /*//////////////////////////////////////////////////////////////
+                                GAME
+  //////////////////////////////////////////////////////////////*/
 
   function updatePosition(uint _x, uint _y) public {
     ship.positionX = _x;
     ship.positionY = _y;
+
+    emit PlayerMove(_x, _y);
   }
 
   function getPosition() public view returns(ShipData memory _ship){
---
+++ src/ships/Ship.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "../BigSky.sol";
+
+contract Ship {
+  BigSky internal immutable bigsky; 
+
+  constructor(BigSky _bigsky) {
+    bigsky = _bigsky;
+  }
+
+}

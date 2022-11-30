204232aa9a60a82cfe0e546fc7b6b22899685a75
bigsky77 <simonjudd2@gmail.com>
2022-11-30 19:32:17 +0200

    feat: updated deploy tests

--- script/DeployBigSky.s.sol
+++ script/DeployBigSky.s.sol
@@ -15,11 +15,6 @@ contract DeployBigSky is Script {
       BigSky bigsky = new BigSky();
       Ship ship = new Ship(bigsky);
       
-      bigsky.launchShip(ship);
-      bigsky.launchShip(ship);
-      bigsky.launchShip(ship);
-      bigsky.launchShip(ship);
-    
       vm.stopBroadcast();
 
       address player2 = vm.addr(0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d);
@@ -41,6 +36,28 @@ contract DeployBigSky is Script {
       
       bigsky.launchShip(ship4);
       bigsky.launchShip(ship5);
+     
+      vm.stopBroadcast();
+      
+      address player4 = vm.addr(0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6);
+      vm.startBroadcast(0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6);
+      
+      Ship ship6 = new Ship(bigsky);
+      Ship ship7 = new Ship(bigsky);
+      
+      bigsky.launchShip(ship6);
+      bigsky.launchShip(ship7);
+     
+      vm.stopBroadcast();
+      
+      address player5 = vm.addr(0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a);
+      vm.startBroadcast(0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a);
+      
+      Ship ship8 = new Ship(bigsky);
+      Ship ship9 = new Ship(bigsky);
+      
+      bigsky.launchShip(ship8);
+      bigsky.launchShip(ship9);
      
       vm.stopBroadcast();
     }

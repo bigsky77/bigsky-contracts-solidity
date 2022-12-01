fddddc5af85da21a9186b1f1cfabc9596cd2f456
bigsky77 <simonjudd2@gmail.com>
2022-12-01 08:25:55 +0200

    test: cleaned up tests

--- test/BigSky.t.sol
+++ test/BigSky.t.sol
@@ -9,47 +9,13 @@ import "../src/ships/ExampleShip.sol";
 contract BigSkyTest is Test {
     BigSky public bigsky;
 
-    address player;
-    address player2;
-    address player3;
-    
     function setUp() public {
         bigsky = new BigSky();
-        
-        player = address(1);
-        vm.label(address(player), 'player1');
-
-        player2 = address(2);
-        vm.label(address(player2), 'player2');
-
-        player3 = address(3);
-        vm.label(address(player3), 'player3');
     }
     
     function testGame() public {
-      
-      vm.startPrank(address(player));
         Ship ship = new Ship(bigsky);
         bigsky.launchShip(ship);
-      vm.stopPrank();
-      
-      vm.startPrank(address(player2));
-        Ship ship2 = new Ship(bigsky);
-        bigsky.launchShip(ship2);
-      vm.stopPrank();
-   
-      vm.startPrank(player3);
-        Ship ship3 = new Ship(bigsky);
-        bigsky.launchShip(ship3);
         
-        Ship ship4 = new Ship(bigsky);
-        bigsky.launchShip(ship4);
-        
-        Ship ship5 = new Ship(bigsky);
-        bigsky.launchShip(ship5);
-      vm.stopPrank();
-   
     }
-
-    
 }
